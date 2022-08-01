fcb9c4b60b485d287b60ec83fb962ab29b412f01
Guruha <95520684+Guruha@users.noreply.github.com>
2022-08-01 12:34:54 +0300

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -10,6 +10,10 @@ while True:
     event, values = window.read()
     # End program if user closes window or
     # presses the OK button
-    if event == "OK" or event == sg.WIN_CLOSED:
+    if event == "Start": 
+        #to_be_continued#
+        
+        
+    if event == sg.WIN_CLOSED:
         break
 window.close()
